8692a12d662fd9613ea75ae47724ed5074f3801b
CBSI-Jenkins-Chimp <CBSI-Jenkins-Chimp@users.noreply.github.com>
2016-05-21 12:26:45 -0700

    Update hello.js
    added simple comment, waiting to merge

--- hello.js
+++ hello.js
@@ -1,2 +1,4 @@
 // console.log("Hello World");
+// this is an edit done from Branch2, hopefully to push in Master right away and trigger a Jenkins build since manual file add does not seem to do it.
+
 This is a sample failure, which I hope will result in a Jenkins build fail.
