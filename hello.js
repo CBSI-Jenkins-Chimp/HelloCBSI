16807bd6c703d92a9dc2bec6c9b7ebf7bc899192
CBSI-Jenkins-Chimp <pierre.johnson@cbsinteractive.com>
2016-05-21 17:31:44 -0700

    this one also should build

--- hello.js
+++ hello.js
@@ -1,4 +1,2 @@
  console.log("Hello World");
-// this is an edit done from Branch2, hopefully to push in Master right away and trigger a Jenkins build since manual file add does not seem to do it.
-
-//This is a sample failure, which I hope will result in a Jenkins build fail.
+// adding a comment to check if repo URL is the same as "project URL"
