606e5d03f96d5ea04841ed5f9f0d75e3b84a2439
CBSI-Jenkins-Chimp <pierre.johnson@cbsinteractive.com>
2016-05-23 16:00:54 -0700

    this should be reflected??2.9

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 // new comment?
  console.log("Hello World");
-// adding a comment to check if repo URL is the same as "project URL" 2.00
+// adding a comment to check if repo URL is the same as "project URL" 3.00
